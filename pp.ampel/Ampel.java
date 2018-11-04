85e7ef6f27b0432cf67b76d566f88a5dad90c7e1
Michael <39065572+Internetfreund@users.noreply.github.com>
2018-11-04 11:23:29 +0100

    Create Ampel.java

---
+++ pp.ampel/Ampel.java
@@ -0,0 +1,12 @@
+package pp.ampel;
+
+public interface Ampel
+{
+    public void schalteRot();
+
+    public void schalteGruen();
+
+    public void passieren();
+
+    public int wartendeFahrzeuge();
+}
